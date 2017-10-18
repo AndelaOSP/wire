c3a84ca8a6a66f409655b8423c5727e1c6fe61bd
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-10-18 14:22:20 +0300

    chore(project setup): add initial configurations - add webpack configurations - add sass modules - add bulma integration - babel configurations [Delivers #152027737]

--- src/reducers/initialState.jsx
+++ src/reducers/initialState.jsx
@@ -1,23 +1,15 @@
 export default {
   incidents: [],
-<<<<<<< HEAD
-=======
-  notes: [],
->>>>>>> chore: extra action type for loading incidents
   isLoading: false,
   hasToken: false,
   error: {
     status: false,
     statusCode: null,
     message: ''
-<<<<<<< HEAD
   },
   selectedIncident: {
     notes: [],
     chats: []
   },
   staff: []
-=======
-  }
->>>>>>> chore: extra action type for loading incidents
 };
