b0b33e12e1664299b904540b7f622e63f73a721c
Akash <akashbaga011@gmail.com>
2018-02-28 17:46:18 +0300

    chore: extra action type for loading incidents
    -Add isLoading property to initial state
    -Add isLoading and ErrorAction action types
    -Add isLoading and Error reducers
    -Display user friendly error messages on error (remove status codes)
    -Add Loading ternary to dashboard render func (fix tests)
    -Implement custom notification component to display incident errors (fix tests)
    -Clean up package.json
    
    [Delivers #155394245]

--- src/reducers/loadingReducer.jsx
+++ src/reducers/loadingReducer.jsx
@@ -1,5 +1,9 @@
 import initialState from './initialState';
+<<<<<<< HEAD
 import { IS_LOADING, ERROR_ACTION, FETCH_INCIDENTS_SUCCESS, FETCH_INCIDENT } from '../actions/actionTypes';
+=======
+import { IS_LOADING, ERROR_ACTION, FETCH_INCIDENTS_SUCCESS } from '../actions/actionTypes';
+>>>>>>> chore: extra action type for loading incidents
 
 const loadingReducer = (state = initialState.isLoading, action) => {
   switch (action.type) {
@@ -9,9 +13,12 @@ const loadingReducer = (state = initialState.isLoading, action) => {
     case FETCH_INCIDENTS_SUCCESS:
       return action.isLoading;
 
+<<<<<<< HEAD
     case FETCH_INCIDENT:
       return action.isLoading;
 
+=======
+>>>>>>> chore: extra action type for loading incidents
     case ERROR_ACTION:
       return !action.status;
 
