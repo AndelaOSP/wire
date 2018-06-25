bf0d382fc0854c70e9abb81d96eb3eefcfd81ed7
Peter Musonye <musonyengigi@gmail.com>
2018-06-25 23:31:32 +0300

    chore: Prevent auto login
    - Currently, if a user is authenticated in another andela app, they can
      visit /dashboard without being redirected to /login for proper auth.
    - Although they do not see any incidents if they lack a valid token from
      the API, /dashboard needs to be protected.

--- src/Components/PrivateRoute/PrivateRoute.Component.jsx
+++ src/Components/PrivateRoute/PrivateRoute.Component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
@@ -10,11 +11,13 @@ import authenticateUser from '../../helpers/auth';
  */
 const PrivateRoute = ({ component: Component, ...rest }) => {
   authenticateUser.authenticate();
+  const { statusCode } = rest;
+  const loginErrors = [401, 403];
   return (
     <Route
       {...rest}
       render={props =>
-        authenticateUser.isAuthenticated ? (
+        authenticateUser.isAuthenticated && loginErrors.indexOf(statusCode) == -1 ? (
           <Component {...props} />
         ) : (
           <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
@@ -29,7 +32,19 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
  */
 PrivateRoute.propTypes = {
   component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]), // Container || functional react component
-  location: PropTypes.object
+  location: PropTypes.object,
+  statusCode: PropTypes.number
 };
 
-export default PrivateRoute;
+/**
+ * map state from the store to props
+ * @param {*} state
+ * @returns {*} partial state
+ */
+const mapStateToProps = state => {
+  return {
+    statusCode: state.error.statusCode
+  };
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
