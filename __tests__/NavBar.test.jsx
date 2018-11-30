c12f7b9423767a5b5476a47185c0c46654ea7fae
Emmanuel Abaye <emmanuel.abaye@andela.com>
2018-11-30 15:46:01 +0100

    test(navbar): increase test for navbar component - add more test to raise coverage to minimum of 85 percent [Finishes #156268389]

--- __tests__/NavBar.test.jsx
+++ __tests__/NavBar.test.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import shallowToJSON from 'enzyme-to-json';
-
 import { NavBar } from '../src/Common/NavBar/NavBar.Component';
+
+jest.mock('../src/helpers/auth');
+
+global.localStorage = {
+  clear: () => {},
+  getItem: () => {},
+};
+
+global.authenticateUser = {
+  isAdmin: () => true ,
+  logoutUser: () => {},
+};
+
+const props = {
+  notify: true,
+  showSearch: true,
+};
 
 describe('Navbar component', () => {
   it('should have all the Navbar content', () => {
-    const navbar = shallow(<NavBar history={{}} />);
+    const navbar = shallow(<NavBar {...props} history={{}} />);
     const tree = shallowToJSON(navbar);
     expect(tree.props.className).toEqual('navbar');
     expect(tree.props.role).toEqual('navigation');
     expect(tree.props['aria-label']).toEqual('main navigation');
     expect(tree.type).toEqual('nav');
   });
+
+  it('should handle sign out', () => {
+    const wrapper = shallow(<NavBar history={{}} />);
+    const signOutSpan = wrapper.find('.dropdown-content span');
+    expect(signOutSpan.length).toEqual(1);
+    signOutSpan.simulate('click', { preventDefault: jest.fn()});
+  });
+
+  it('should handle search', () => {
+    const wrapper = shallow(<NavBar {...props} history={{ push: ()=> {}}} />);
+    const searchInput = wrapper.find('input.search-input');
+    expect(searchInput.length).toEqual(1);
+    const spy = jest.spyOn(wrapper.instance().props.history, 'push');
+    searchInput.simulate('focus', { preventDefault: jest.fn()});
+    expect(spy).toHaveBeenCalled();
+  });
+
 });
+
+
+
