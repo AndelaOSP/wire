6ed853033a6015b4d9af95e317958fc09832f193
nzaujk <joseph.nzau@andela.com>
2018-12-11 10:49:43 +0300

    add slicker

--- src/testHelpers.jsx
+++ src/testHelpers.jsx
@@ -1,5 +1,5 @@
 import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'; /* eslint-disable no-shadow */
 import { ERROR_ACTION, IS_LOADING } from './actions/actionTypes';
 
 export const mockStore = (initialState = {}) => configureMockStore([thunk])(initialState);
