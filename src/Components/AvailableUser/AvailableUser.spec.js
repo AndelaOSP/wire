76d1325bf8e2f3da72c72b53fbb73f5b4cea61e4
nzaujk <joseph.nzau@andela.com>
2018-12-11 12:04:41 +0300

    fix lint errors

--- src/Components/AvailableUser/AvailableUser.spec.js
+++ src/Components/AvailableUser/AvailableUser.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import sinon from 'sinon';
-import { configure, shallow, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import AvailableUser from './AvailableUser.Component';
 
