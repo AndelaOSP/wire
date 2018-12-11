d490e0a45dffc02b29c548e0f27b197fb184accd
nzaujk <joseph.nzau@andela.com>
2018-12-11 12:03:26 +0300

    write tests

--- src/Components/AvailableUser/AvailableUser.spec.js
+++ src/Components/AvailableUser/AvailableUser.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sinon from 'sinon';
 import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import AvailableUser from './AvailableUser.Component';
@@ -8,6 +9,8 @@ configure({ adapter: new Adapter() });
 describe('should render <AvailableUser />', () => {
   let props;
   let wrapper;
+  const handleRemove = sinon.spy();
+  const handlePositionChange = sinon.spy();
   beforeEach(() => {
     props = {
       id: '',
@@ -16,9 +19,8 @@ describe('should render <AvailableUser />', () => {
       username: '',
       role: '',
       country: '',
-      handlePositionChange: jest.fn(),
-      handleRemove: jest.fn(),
-
+      handlePositionChange,
+      handleRemove,
     };
     if (wrapper) wrapper.unmount();
   });
@@ -29,5 +31,24 @@ describe('should render <AvailableUser />', () => {
   it('should render with 8 props', () => {
     wrapper = shallow(<AvailableUser {...props} />);
     expect(Object.keys(wrapper.instance().props).length).toEqual(8);
+  });
+  it('should render SelectField', () => {
+    wrapper = shallow(<AvailableUser {...props} />);
+    expect(wrapper.find('SelectField').length).toEqual(1);
+  });
+  it('should render MenuItems', () => {
+    wrapper = shallow(<AvailableUser {...props} />);
+    expect(wrapper.find('MenuItem').length).toEqual(2);
+  });
+  it('should renders handleRemove', () => {
+    wrapper = shallow(<AvailableUser {...props} prepareStyles="" />);
+    wrapper.find('div').at(4).simulate('click');
+    expect(handleRemove.called).toEqual(true);
+  });
+  it('should renders handlePositionChange', () => {
+    wrapper = shallow(<AvailableUser {...props} prepareStyles="" />);
+    const event = { target: { name: 'position ', value: 'new position' } };
+    wrapper.find('SelectField').simulate('change', event);
+    expect(handlePositionChange.called).toEqual(true);
   });
 });
